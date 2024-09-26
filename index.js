4bb99ac613a4bee7770d0bec45e3a3022b84f67d
Aryan Khandelwal <aryankhandelwal0127@gmail.com>
2024-09-26 20:58:10 +0530

    added cors config

--- index.js
+++ index.js
@@ -32,7 +32,11 @@ app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
+app.use(cors({
+    origin: "*", // Allow requests from any origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow these methods
+    credentials: true, // Allow credentials (e.g., cookies)
+  }));
 app.use("/assets", express.static(path.join(__dirname,'public/assets')));
 
 //file storage
